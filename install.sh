4ee35499d6d99427a891d060014ed5b635ecf874
Micah Breedlove <micah.breedlove@iostudio.com>
2011-11-30 15:19:04 -0600

    added install and web_install scripts

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ -z "$1" ] || [ "${1}" = "--no-io" ]; then
+  IO="NO"
+else
+  IO="YES"
+fi
+echo "Setting up Bash the way you like it (Part 1)"
+cd skel
+for f in $(ls DOT.*)
+do
+  mv $f $HOME/`echo $f | sed 's/DOT//'`
+done
+echo "Copy your bin/ files as needed."
+ if [ $IO == "YES" ]; then
+if [ -z $1 ] || [ $1 != "--no-io" ]; then
+  echo "Performing git Magic-ness"
+  git submodule update --init --recursive
+  cd ../
+  echo "Setting up Bash the way you like it: The sequel" 
+  cp ioBashExtras $HOME
+  cd $HOME/ioBashExtras
+  ./install.sh $HOME/.bashrc
+fi
+echo "#######################################################"
+echo "#                                                     #"
+echo "#       DON'T FORGET TO UPDATE YOUR .gitconfig !      #"
+echo "#                                                     #"
+echo "#######################################################"
---
+++ web_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd $HOME
+git clone git@github.com:druid628/dotDotFiles.git
+cd dotDotFiles
+./install.sh
