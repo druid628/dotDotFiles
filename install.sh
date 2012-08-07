3b1965edeea9d0042f70c80b3898ea96a17304b7
Micah Breedlove <druid628@gmail.com>
2012-08-07 15:17:58 -0500

    finally got stuff working correctly

--- install.sh
+++ install.sh
@@ -8,16 +8,22 @@ echo "Setting up Bash the way you like it (Part 1)"
 cd skel
 for f in $(ls DOT.*)
 do
-  mv $f $HOME/`echo $f | sed 's/DOT//'`
+  NEWFILENAME=`echo $f | sed 's/DOT//'`
+  echo $NEWFILENAME
+  if [ -e $HOME/$NEWFILENAME ]; then
+      mv $HOME/$NEWFILENAME $HOME/$NEWFILENAME.bak
+  fi
+  ln -s `pwd`/$f $HOME/$NEWFILENAME
 done
 echo "Copy your bin/ files as needed."
- if [ $IO == "YES" ]; then
-if [ -z $1 ] || [ $1 != "--no-io" ]; then
-  echo "Performing git Magic-ness"
-  git submodule update --init --recursive
-  cd ../
-  echo "Setting up Bash the way you like it: The sequel" 
-  cp ioBashExtras $HOME
-  cd $HOME/ioBashExtras
-  ./install.sh $HOME/.bashrc
-fi
+#  if [ $IO == "YES" ]; then
+#     if [ -z $1 ] || [ $1 != "--no-io" ]; then
+#       echo "Performing git Magic-ness"
+#       git submodule update --init --recursive
+#       cd ../
+#       echo "Setting up Bash the way you like it: The sequel" 
+#       cp ioBashExtras $HOME
+#       cd $HOME/ioBashExtras
+#       ./install.sh $HOME/.bashrc
+#     fi
+#  fi
